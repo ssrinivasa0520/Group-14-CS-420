5d53b3c187b6307012017ad52dd856d86d891912
blazinsp <shri98@uab.edu>
2023-10-30 10:38:46 -0500

    Changes

--- src/main/java/com/javafx/farmdashboard/helpers/ItemCreator.java
+++ src/main/java/com/javafx/farmdashboard/helpers/ItemCreator.java
@@ -38,7 +38,6 @@ public class ItemCreator {
         return createItem(Constants.FARM_WIDTH / 2, Constants.FARM_LENGTH / 2);
     }
 
-
     public static ItemI createItem(double posX, double posY) {
         return createItem(posX, posY, Constants.ItemType.DEFAULT, false);
     }
